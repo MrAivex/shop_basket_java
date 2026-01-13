db381c4c37f8b3d6469e0ec4f31b2375a8e7b10c
GrafAleksey315 <Lekstar_67@mail.ru>
2026-01-13 23:46:29 +0300

    #3

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -3,27 +3,23 @@ import java.util.ArrayList;
 public class Cart {
     private ArrayList<Product> products;
 
-    // Конструктор
     public Cart() {
         this.products = new ArrayList<>();
     }
 
-    // Добавить товар
     public void addProduct(Product product) {
         products.add(product);
     }
 
-    // Удалить товар по названию
     public void removeProduct(String productName) {
         for (int i = 0; i < products.size(); i++) {
             if (products.get(i).getName().equalsIgnoreCase(productName)) {
                 products.remove(i);
-                break; // Удаляем только первый найденный товар
+                break;
             }
         }
     }
 
-    // Посчитать общую сумму
     public double getTotal() {
         double total = 0;
         for (Product product : products) {
@@ -32,7 +28,6 @@ public class Cart {
         return total;
     }
 
-    // Вывести содержимое корзины
     public void printCart() {
         System.out.println("=== Корзина ===");
         if (products.isEmpty()) {
