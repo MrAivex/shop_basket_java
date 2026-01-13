27bc8cbd2ec081d3fde906edd6f98281fb168d92
GrafAleksey315 <Lekstar_67@mail.ru>
2026-01-13 23:47:55 +0300

    #5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        // Создаем корзину
         Cart cart = new Cart();
 
-        // Создаем товары
         Product laptop = new Product("Ноутбук", 75000.0);
         Product mouse = new Product("Мышка", 1500.0);
         Product keyboard = new Product("Клавиатура", 3000.0);
         Product headphones = new Product("Наушники", 5000.0);
 
-        // Добавляем товары в корзину
         cart.addProduct(laptop);
         cart.addProduct(mouse);
         cart.addProduct(keyboard);
 
-        // Показываем корзину
         cart.printCart();
 
-        // Удаляем товар
         System.out.println("Удаляем: Мышка\n");
         cart.removeProduct("Мышка");
 
-        // Снова показываем корзину
         cart.printCart();
 
-        // Дополнительные тесты
         System.out.println("=== Дополнительные тесты ===");
 
-        // Добавляем еще товар
         cart.addProduct(headphones);
         cart.printCart();
 
-        // Пытаемся удалить несуществующий товар
         cart.removeProduct("Монитор");
         cart.printCart();
 
-        // Очищаем корзину
         cart.removeProduct("Ноутбук");
         cart.removeProduct("Клавиатура");
         cart.removeProduct("Наушники");
