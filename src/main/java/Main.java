281f04f48469ac01db72f82e36b855a4432f2c45
GrafAleksey315 <Lekstar_67@mail.ru>
2026-01-13 23:56:49 +0300

    #6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,30 @@ public class Main {
     public static void main(String[] args) {
         Cart cart = new Cart();
 
-        Product laptop = new Product("Ноутбук", 75000.0);
-        Product mouse = new Product("Мышка", 1500.0);
-        Product keyboard = new Product("Клавиатура", 3000.0);
-        Product headphones = new Product("Наушники", 5000.0);
+        Product naushniki = new Product("Наушники BOSE", 30000.0);
+        Product kovrik = new Product("Коврик для мыши", 1200.0);
+        Product culler = new Product("Куллер", 300.0);
+        Product Claviatura = new Product("Клавиатура", 3000.0);
 
-        cart.addProduct(laptop);
-        cart.addProduct(mouse);
-        cart.addProduct(keyboard);
-
-        cart.printCart();
-
-        System.out.println("Удаляем: Мышка\n");
-        cart.removeProduct("Мышка");
+        cart.addProduct(naushniki);
+        cart.addProduct(kovrik);
+        cart.addProduct(culler);
 
         cart.printCart();
 
-        System.out.println("=== Дополнительные тесты ===");
+        System.out.println("Удаляем: Наушники BOSE\n");
+        cart.removeProduct("Наушники BOSE");
 
-        cart.addProduct(headphones);
         cart.printCart();
 
-        cart.removeProduct("Монитор");
+        cart.addProduct(Claviatura);
         cart.printCart();
 
-        cart.removeProduct("Ноутбук");
+        cart.removeProduct("Коврик для мыши");
+        cart.printCart();
+
+        cart.removeProduct("Куллер");
         cart.removeProduct("Клавиатура");
-        cart.removeProduct("Наушники");
         cart.printCart();
     }
 }
